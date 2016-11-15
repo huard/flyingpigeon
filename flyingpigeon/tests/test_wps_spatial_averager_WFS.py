1f4aeff9591e2eeffc3f36a6f4191706638aa379
tomLandry <tom.landry@crim.ca>
2016-11-15 13:45:46 -0500

    First version os averager and its test

---
+++ flyingpigeon/tests/test_wps_spatial_averager_WFS.py
@@ -0,0 +1,12 @@
+import pytest
+
+from .common import WpsTestClient, TESTDATA, assert_response_success
+
+def test_wps_subset_WFS():
+    wps = WpsTestClient()
+    datainputs = "[resource={0};typename=usa:states;featureids=states.49;mosaic=False]".format(
+       TESTDATA['cmip5_tasmax_2006_nc'])
+    resp = wps.get(service='wps', request='execute', version='1.0.0',
+                   identifier='spatial_averager_WFS',
+                   datainputs=datainputs)
+    assert_response_success(resp)
