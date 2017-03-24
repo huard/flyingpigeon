c082e385f04df3d855f24e984e0fba5789c3d353
dbyrns <david.byrns@crim.ca>
2017-03-24 11:03:26 -0400

    Multiple changes on wps_subset_WPS (Reported from commit b512e5b3abc64da983e90a7eff1ed25c2652633c from old_master)
    1) More tests on polygones to elucidate geometries problems
    2) Modification of the number of possible outputs
    3) Simplify geometries while processing : helps most of the time to get rid of malform geometries
    4) Mark most of the other tests as 'skip'

--- flyingpigeon/processes/wps_subset_WFS.py
+++ flyingpigeon/processes/wps_subset_WFS.py
@@ -4,18 +4,38 @@ import zipfile
 import logging
 import shutil
 import subprocess
+import numpy as np
+
 
 from flyingpigeon.subset import clipping
 from flyingpigeon import config
 from pywps.Process import WPSProcess
 from owslib.wfs import WebFeatureService
 
+from netCDF4 import Dataset
+from netCDF4 import MFDataset
+import fiona
+from shapely.geometry import shape, mapping
+
+from osgeo import ogr
+
 logger = logging.getLogger(__name__)
+
+def Polygonize(geom, geoms_list):
+    if geom.GetGeometryType() is ogr.wkbPolygon:
+        geoms_list.append(geom.Clone())
+    elif geom.GetGeometryType() is ogr.wkbMultiPolygon:
+        for i in range(0, geom.GetGeometryCount()):
+            geom2 = geom.GetGeometryRef(i)
+            if geom2.GetGeometryType() is ogr.wkbPolygon:
+                geoms_list.append(geom2.Clone())
+            elif geom2.GetGeometryType() is ogr.wkbMultiPolygon:
+                Polygonize(geom2.GetGeometryRef(i), geoms_list)
 
 class WFSClippingProcess(WPSProcess):
     def __init__(self):
         WPSProcess.__init__(
-            self, 
+            self,
             identifier="subset_WFS",
             title="Subset WFS",
             version="0.1",
@@ -33,7 +53,7 @@ class WFSClippingProcess(WPSProcess):
             maxmegabites=5000,
             formats=[{"mimeType":"application/x-netcdf"}],
             )
-       
+
         self.typename = self.addLiteralInput(
             identifier="typename",
             title="TypeName",
@@ -51,7 +71,7 @@ class WFSClippingProcess(WPSProcess):
             minOccurs=0,
             maxOccurs=1
         )
-        
+
         self.xmlfilter = self.addLiteralInput(
             identifier="filter",
             title="XML filter",
@@ -89,13 +109,26 @@ class WFSClippingProcess(WPSProcess):
             identifier="output",
         )
 
-        self.output_netcdf = self.addComplexOutput(
+        # self.output_netcdf = self.addComplexOutput(
+        #     title="Subsets for one dataset",
+        #     abstract="NetCDF file with subsets of one dataset.",
+        #     formats=[{"mimeType":"application/x-netcdf"}],
+        #     asReference=True,
+        #     identifier="ncout",
+        # )
+
+        self.outputs_netcdf = []
+        k = np.arange(0,100)
+        for i in k:
+            self.outputs_netcdf.append(self.addComplexOutput(
             title="Subsets for one dataset",
             abstract="NetCDF file with subsets of one dataset.",
-            formats=[{"mimeType":"application/x-netcdf"}],
+            formats=[{"mimeType": "application/x-netcdf"}],
             asReference=True,
-            identifier="ncout",
-        )
+            identifier="ncout_{}".format(i),
+        ))
+
+        t = 0
 
     def execute(self):
         urls = self.getInputValues(identifier='resource')
@@ -104,13 +137,12 @@ class WFSClippingProcess(WPSProcess):
         xmlfilter = self.xmlfilter.getValue()
         typename = self.typename.getValue()
         variable = self.variable.getValue()
-
         logger.info('urls = %s', urls)
         logger.info('filter = %s', xmlfilter)
         logger.info('mosaic = %s', mosaic)
         logger.info('typename = %s', typename)
         logger.info('featureids = %s', featureids)
-    
+
         self.status.set('Arguments set for WPS subset process', 0)
         logger.debug('starting: num_files=%s' % (len(urls)))
 
@@ -135,8 +167,10 @@ class WFSClippingProcess(WPSProcess):
 
             #get unique name for folder and create it
             unique_dirname = str(uuid.uuid4())
+            #unique_dirname = 'toto'
             dirpath = os.path.join(config.cache_path(), unique_dirname)
-            os.mkdir(dirpath)
+            if not  os.path.exists(dirpath):
+                os.mkdir(dirpath)
             filepath = os.path.join(dirpath, 'file.zip')
 
             #Saves the result in folder and unzips it
@@ -151,8 +185,11 @@ class WFSClippingProcess(WPSProcess):
             source_shp_path = os.path.join(dirpath, source_shapefile_name + ".shp")
             dest_shapefile_name =source_shapefile_name + "_flipped"
             dest_shp_path = os.path.join(dirpath, dest_shapefile_name + ".shp")
+
+            print dest_shp_path
+            print source_shp_path
             args = ("ogr2ogr", "-s_srs", "+proj=latlong +datum=WGS84 +axis=neu +wktext",
-                    "-t_srs", "+proj=latlong +datum=WGS84 +axis=enu +wktext",  dest_shp_path, source_shp_path)
+                    "-t_srs", "+proj=latlong +datum=WGS84 +axis=enu +wktext", "-simplify", "0.0001", dest_shp_path, source_shp_path)
             output, error = subprocess.Popen(args, stdout=subprocess.PIPE,
                                              stderr=subprocess.PIPE).communicate()
             logger.info('ogr2ogr info:\n %s ' % output)
@@ -167,7 +204,7 @@ class WFSClippingProcess(WPSProcess):
                 variable=variable,
                 dir_output=os.path.abspath(os.curdir),
                 geomcabinet=dirpath,
-                geom=dest_shapefile_name
+                geom=dest_shp_path
                 )
             logger.info('WPS clipping done')
         except Exception as e:
@@ -190,8 +227,21 @@ class WFSClippingProcess(WPSProcess):
 
         self.output.setValue(tarf)
 
-        i = next((i for i, x in enumerate(results) if x), None)
-        self.output_netcdf.setValue(results[i])
+        l = len(self.outputs_netcdf)
+        j = min(l,len(results))
+
+        jj = np.arange(0,j)
+        for jjj in jj:
+            self.outputs_netcdf[jjj].setValue(results[jjj])
+
+
+
+        #i = next((i for i, x in enumerate(results) if x), None)
+        #self.output_netcdf.setValue(results[i])
+
+
+
+        #self.output_netcdf2.setValue(results[1])
 
         self.status.set('done', 100)
 
